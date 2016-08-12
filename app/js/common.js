0da6f2d0a5451e64c5999b02b52406dfb667a2f3
Victor Oberenko <oberemkovictor@gmail.com>
2016-08-12 18:30:43 +0300

    finally version on friday

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,9 @@
 $(document).ready(function(){
 
+  $('.js-number').inputmask("8 (999) 999-99-99");
+
+  $('.parallax').parallax({imageSrc: 'img/third_screen.png'});
+
   function fixedHeader() {
     var header = $('.site-header');
     var topBackground = $('.main-header');
@@ -10,10 +14,8 @@ $(document).ready(function(){
 
       if(headerHeight < y) {
         header.addClass('site-header--active');
-        topBackground.addClass('main-header--active');
       } else {
         header.removeClass('site-header--active');
-        topBackground.removeClass('main-header--active');
       }
     });
   }
@@ -57,18 +59,115 @@ $(document).ready(function(){
   function flyPopup() {
     var flyPopup = $('.cost-popup');
     var forPopup = flyPopup.offset().top - flyPopup.height();
+    var items = $('.cost-items');
 
     window.addEventListener('scroll', function(e){
       var y = window.scrollY;
+      console.info(y);
+      console.log(items.offset().top)
 
-      if(y > forPopup) {
+      if(y > forPopup && y < (items.offset().top + items.height() - 500)) {
         flyPopup.css('top', (y - forPopup) + 'px');
+      } else if(y > (items.offset().top + items.height() - 500)) {
+        flyPopup.css('top', y < (items.offset().top + items.height() - 499) + 'px');
       } else {
         flyPopup.css('top', 0 + 'px');
       }
     });
   }
 
+  function activePopup(){
+
+    // открытие попапа
+
+    $('.js-button').on('click', function(){
+      $('.main-popup .title').text('Записаться на прием');
+      $('.main-popup').addClass('main-popup--active');
+      $('.main-popup-background').addClass('main-popup-background--active');
+    });
+
+    $('.js-call').on('click', function(){
+      $('.main-popup .title').text('Перезвоните мне');
+      $('.main-popup').addClass('main-popup--active');
+      $('.main-popup-background').addClass('main-popup-background--active');
+    });
+
+    // закрытие попапа
+
+    $('.main-popup-background').on('click', function(){
+      $('.main-popup').removeClass('main-popup--active');
+      document.querySelector('.main-form').children[1].classList.remove('error');
+      document.querySelector('.main-form').children[2].classList.remove('error');
+      $('.main-popup-background').removeClass('main-popup-background--active');
+    });
+  };
+
+  function sliderTechnic() {
+    var block = document.querySelector('.photos-items');
+    var imageBlock = document.querySelector('.machine-content .image');
+    console.log(imageBlock);
+
+    block.addEventListener('click', function(e){
+
+      if(e.target != this) {
+        for(var i = 0; i < block.children.length; i++) {
+          block.children[i].classList.remove('item--active');
+        }
+      }
+
+      if(e.target.parentElement == this.children[0]) {
+        imageBlock.classList = "";
+        imageBlock.classList.add('image-1');
+        this.children[0].classList.add('item--active');
+      } else if (e.target.parentElement == this.children[1]) {
+        imageBlock.classList = "";
+        imageBlock.classList.add('image-2');
+        this.children[1].classList.add('item--active');
+      } else if (e.target.parentElement == this.children[2]) {
+        imageBlock.classList = "";
+        imageBlock.classList.add('image-3');
+        this.children[2].classList.add('item--active');
+      }
+
+    });
+  };
+
+  function validationForms() {
+    var mainForm = document.querySelector('.main-form');
+    var costForm = document.querySelector('.cost-form');
+
+    mainForm.addEventListener('submit', function(e) {
+
+      this.children[1].classList.remove('error');
+      this.children[2].classList.remove('error');
+
+      if(this.children[1].value.length < 3) {
+        e.preventDefault();
+        this.children[1].classList.add('error');
+      } else if(this.children[2].value.length < 11) {
+        e.preventDefault();
+        this.children[2].classList.add('error');
+      }
+    });
+
+    costForm.addEventListener('submit', function(e) {
+
+      this.children[1].classList.remove('error');
+      this.children[2].classList.remove('error');
+
+      if(this.children[1].value.length < 3) {
+        e.preventDefault();
+        this.children[1].classList.add('error');
+      } else if(this.children[2].value.length < 11) {
+        e.preventDefault();
+        this.children[2].classList.add('error');
+      }
+    });
+  }
+
+  validationForms();
+  sliderTechnic();
+  activePopup();
   flyPopup();
   gallery();
   scrollingMenu();
